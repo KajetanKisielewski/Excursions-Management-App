1558fac76ef1f6e2e2bb8686720360902c64fa34
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-08-31 18:46:20 +0200

    .

--- src/js/admin.js
+++ src/js/admin.js
@@ -2,4 +2,36 @@ import './../css/admin.css';
 
 import ExcursionsAPI from './ExcursionsAPI';
 
-console.log('admin');
+
+const init = () => {
+    console.log('admin');
+    addExcursions()
+}
+
+const addExcursions = () => {
+
+    const form = document.querySelector('.form');
+    console.log(form);
+
+    form.addEventListener('submit' , e => {
+        e.preventDefault();
+
+        console.log(e.currentTarget.elements)
+
+        const { name , description , adultPrice , childrenPrice } = e.target.elements;
+
+        const data = {
+            name: name.value,
+            description: description.value,
+            adultPrice: adultPrice.value,
+            childrenPrice: childrenPrice.value
+        };
+
+        console.log(data)
+    })
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded' , init)
